443e79419af90c00bb32724f7b11457cdbafb560
saifyedes <saifyedes20@gmail.com>
2025-04-24 14:08:25 +0100

    Environment de travail

---
+++ app/src/main/java/com/example/moitoi/AnswerQuizActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moitoi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AnswerQuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer_quiz)
+    }
+}
