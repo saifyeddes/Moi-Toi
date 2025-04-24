5e3f35e3aa66ceb5a3a0246251bbd081dca702e1
saifyedes <saifyedes20@gmail.com>
2025-04-24 21:49:40 +0100

    Firebase Connect

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,7 +41,8 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,6 +52,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/moitoi/MatchActivity.java
+++ app/src/main/java/com/example/moitoi/MatchActivity.java
@@ -1,22 +1,40 @@
 package com.example.moitoi;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MatchActivity extends AppCompatActivity {
+    FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_match);
+        setContentView(R.layout.activity_match); // assure-toi que le layout existe aussi
+        firestore=FirebaseFirestore.getInstance();
+        Map<String,Object> users=new HashMap<>();
+        users.put("firstname","EASY");
+        users.put("lastname","TUTO");
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(),"Succes",Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        findViewById(R.id.layoutMan).setOnClickListener(v ->
-                Toast.makeText(this, "Homme sélectionné", Toast.LENGTH_SHORT).show()
-        );
-
-        findViewById(R.id.layoutWoman).setOnClickListener(v ->
-                Toast.makeText(this, "Femme sélectionnée", Toast.LENGTH_SHORT).show()
-        );
     }
 }
