f79d995f3cb975191c32271977d7215a08a3b8da
Rouahadjhassine <hadjhassineroua19@gmail.com>
2025-04-29 21:40:36 +0100

    enregistrer les quiz et user responses dans BD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore-ktx") // Firebase Firestore
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
